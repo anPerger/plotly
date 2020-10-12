2fd3f2504bc28f5f20d5f7ea206a30e1cd777cb4
audric <audricperger@gmail.com>
2020-10-12 11:18:46 -0700

    yikes

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ function getSampleID (data, id) {
         }
     }
 }
+// function getAllSamples (data) {
+//     for(const samples in data) {
+//         samples === 
+//     }
+// }
 // retrieve metadata id function
 function getMetaID (data, id) {
     for(const metaData in data) {
@@ -34,17 +39,52 @@ function makeChart (data) {
     let trace = {
         x: data.otuIDs,
         y: data.top10,
-        type: "bar"
+        type: 'bar'
 
     };
     let layout = {
-        title: "top 10 bacteria",
-        xaxis: { title: "bacteria" },
-        yaxis: { title: "amount"}
+        title: 'top 10 bacteria',
+        xaxis: { title: 'bacteria' },
+        yaxis: { title: 'amount'}
       };
 
     let barTrace = [trace];
-    Plotly.newPlot("bar", barTrace, layout)
+    Plotly.newPlot('bar', barTrace, layout)
+}
+// function makeTable (data, id) {
+//     let data = [{
+//         type: 'table',
+//         header: data,
+//         values: data
+//     }]
+//     Plotly.newPlot('sample-metadata', data)
+// }
+
+function makeBubble (data) {
+    data.otuIDs = data.otu_ids;
+    data.values = data.sample_values;
+    let trace1 = {
+        x: data.otuIDs,
+        y: data.values,
+        mode: 'markers',
+        marker: {
+          color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+          opacity: [1, 0.8, 0.6, 0.4],
+          size: [40, 60, 80, 100]
+        }
+      };
+      
+    let demo = [trace1];
+      
+    let layout = {
+        title: 'Marker Size and Color',
+        showlegend: false,
+        height: 600,
+        width: 600
+      };
+      
+    Plotly.newPlot('bubble', demo, layout);
+
 }
 d3.json('samples.json').then((samples) => {
     // console.log(importedData);
@@ -75,13 +115,13 @@ d3.json('samples.json').then((samples) => {
         const selectionSample = getSampleID(dataSamples, selection);
         const metaSample = getMetaID(metaData, selection);
         console.log(metaSample)
-        // console.log(selectionSample)
+        console.log(selectionSample)
         
         const sortedSample = sortSlice(selectionSample);
         // console.log(sortedSample)
-
-        makeChart(sortedSample)
-        
+        makeBubble(selectionSample);
+        makeChart(sortedSample);
+        // makeTable(metaSample)
     });
 });
 
